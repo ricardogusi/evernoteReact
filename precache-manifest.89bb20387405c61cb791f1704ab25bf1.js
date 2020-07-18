3e6ea184dcb2aa7d2babbdf3637769e2010e80da
ricardogusi <ricardogusi@hotmail.com>
2020-07-18 10:19:26 -0300

    Updates

---
+++ precache-manifest.89bb20387405c61cb791f1704ab25bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b912bb7a57e94fba5ee34b540aa187",
+    "url": "/evernoteReact/index.html"
+  },
+  {
+    "revision": "69a414d8ce0d4221de7c",
+    "url": "/evernoteReact/static/css/main.60d5545d.chunk.css"
+  },
+  {
+    "revision": "94a6f98e213d54ba9308",
+    "url": "/evernoteReact/static/js/2.28caa4e8.chunk.js"
+  },
+  {
+    "revision": "e66d8bfcf01b9d69124b67e3e620fab0",
+    "url": "/evernoteReact/static/js/2.28caa4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a414d8ce0d4221de7c",
+    "url": "/evernoteReact/static/js/main.691564d8.chunk.js"
+  },
+  {
+    "revision": "0b286a4b4959f8ec2503",
+    "url": "/evernoteReact/static/js/runtime-main.c78a40d2.js"
+  }
+]);

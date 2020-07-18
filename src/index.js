044170614336b8d3d4dfe72e620e361b10085c18
ricardogusi <ricardogusi@hotmail.com>
2020-07-18 10:16:53 -0300

    primeiro commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const firebase = require("firebase");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAfNoz3CY8cJcjR30WpP41maZONWWpV9FI",
+  authDomain: "evernotereact.firebaseapp.com",
+  databaseURL: "https://evernotereact.firebaseio.com",
+  projectId: "evernotereact",
+  storageBucket: "evernotereact.appspot.com",
+  messagingSenderId: "280669860495",
+  appId: "1:280669860495:web:2f2800cd5d78ab5e25f6fa",
+  measurementId: "G-NY3NCG2JLN",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
